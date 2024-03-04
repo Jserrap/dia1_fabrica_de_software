e6d37bbc1adb3be0c01dbc6739d16863738392d9
Jserrap <joaoserrap@hotmail.com>
2024-03-04 17:08:55 -0300

    :sparkles: Resolução atividade 2

---
+++ atividade2.py
@@ -0,0 +1,8 @@
+# Fazer uma lista e um dicionario aplicando todos os tipos de variaveis (int, float, string e boolean) e depois imprimir na tela a lista e o dicionario
+lista = [1, 4.3, 'texto', True]
+dicionario = {1:'um', 3.4:'Tres ponto quatro', 'seis': 6, False: 'falso'}
+print(f"Lista => {lista} // Dicionário => {dicionario}")
+
+# Fazer duas operações matematicas, uma com inteiros, e outra com float, imprimindo os resultados na tela
+print(1 + 4)
+print(4.7 + 5.3)
